4b329f609e94075643bcb87e65f8a658683ebaf1
bbadaso <badasobarento@gmail.com>
2021-12-02 04:10:50 +0000

    input

---
+++ input.js
@@ -0,0 +1,36 @@
+const { connect } = require("./client");
+let conn = 0 
+
+const setupInput = function (connection) {
+ conn = connection
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+
+process.exit();
+  }
+ if (key === 'w' ) {
+   conn.write("Move: up")
+ }
+ if (key === "a"); {
+  conn.write("Move: left")
+ }
+ if (key === "s");{
+  conn.write("Move: down")
+ }
+ if (key === "d");{
+  conn.write("Move: right")
+ }
+
+ }
+
+  // your code here
+
+module.exports = {setupInput, handleUserInput}
