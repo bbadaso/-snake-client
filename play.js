46544fbb3954afd8af4d192520982c840da2b963
bbadaso <badasobarento@gmail.com>
2021-12-02 00:57:33 +0000

    play

--- play.js
+++ play.js
@@ -2,22 +2,8 @@
 
 // establishes a connection with the game server
 const {connect} = require("./client");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key = '\u0003')
-    process.exit();
-  // your code here
-  console.log();
-};
+ 
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
